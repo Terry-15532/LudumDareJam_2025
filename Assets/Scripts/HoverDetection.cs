c45a5dcee7327309b6b3c5a6492f6c720da18ed8
wang4040 <jerry2000wang@yahoo.ca>
2025-04-06 12:14:19 +0800

    Updated QTE and Main Menu

---
+++ Assets/Scripts/HoverDetection.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverDetection : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Vector3 minScale = Vector3.one;
+    public Vector3 maxScale = new Vector3(1.2f, 1.2f, 1.2f);
+    public float scaleSpeed = 5f;
+
+    private Vector3 targetScale;
+    private RectTransform rectTransform;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        targetScale = minScale;
+        rectTransform.localScale = minScale;
+    }
+
+    void Update()
+    {
+        rectTransform.localScale = Vector3.Lerp(rectTransform.localScale, targetScale, Time.deltaTime * scaleSpeed);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        targetScale = maxScale;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        targetScale = minScale;
+    }
+}
