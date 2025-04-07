98a26df6f9cf869dc28e571af1614d29cbaff8c4
wang4040 <jerry2000wang@yahoo.ca>
2025-04-08 01:56:28 +0800

    Intro

--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -13,15 +13,22 @@ public class StartGameButton : Button{
 	//public Lines[] lines;
 	//public HoverDetection exit;
 	public CustomUIElement fade;
-	public GameObject intro;
-	public VideoPlayer introPlayer;
+	//public GameObject intro;
+	//public VideoPlayer introPlayer;
 	public bool firstTime;
 	private Image img;
 	private int emissionID = Shader.PropertyToID("_Emission");
 	private int colorID = Shader.PropertyToID("_EdgeColor");
+	public Animator line1;
+	public float time1;
+	public Animator line2;
+    public float time2;
+    public Animator line3;
+    public float time3;
 
 
-	public override void OnMouseEnter(){
+
+    public override void OnMouseEnter(){
 		base.OnMouseEnter();
 		SoundSys.PlaySound("Pop");
 		img.material.SetColor(colorID, new Color(0.95f, 0.6f, 0.7f));
@@ -75,15 +82,30 @@ public class StartGameButton : Button{
 
 		yield return new WaitForSeconds(0.6f);
 
-		intro.SetActive(true);
-		introPlayer.Play();
+		line1.Play("TextFadeIn");
+        yield return new WaitForSeconds(time1);
+        line1.Play("TextFadeOut");
+        yield return new WaitForSeconds(0.5f);
+        line2.Play("TextFadeIn");
+        yield return new WaitForSeconds(time2);
+        line2.Play("TextFadeOut");
+        yield return new WaitForSeconds(1.5f);
 
-		yield return new WaitForSeconds(1f);
+		SoundSys.PlaySound("stomach_growl");
+        yield return new WaitForSeconds(0.5f);
+        line3.Play("TextFadeIn");
+        yield return new WaitForSeconds(time3);
+        line3.Play("TextFadeOut");
+        yield return new WaitForSeconds(1f);
+        //intro.SetActive(true);
+        //introPlayer.Play();
 
-		while (introPlayer.isPlaying)
-			yield return null;
+        //yield return new WaitForSeconds(1f);
 
-		intro.SetActive(false);
-		SceneSwitching.SwitchTo("1");
+        //while (introPlayer.isPlaying)
+        //	yield return null;
+
+        //intro.SetActive(false);
+        SceneSwitching.SwitchTo("1");
 	}
 }
